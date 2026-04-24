3c7f3982ea3349658b572d104cab5cabd778e26f
zh <2088376133@qq.com>
2026-04-24 12:43:23 +0800

    提交部分

--- hci/user-in-the-box/uitb/tasks/__init__.py
+++
@@ -1,7 +0,0 @@
-from .remote_driving.RemoteDriving import RemoteDriving
-from .pointing.Pointing import Pointing
-from .small_object_touch.SmallObjectTouch import SmallObjectTouch
-from .tracking.Tracking import Tracking
-from .choice_reaction.ChoiceReaction import ChoiceReaction
-from .low_level_controller.LowLevelController import LowLevelController
-from .unity.Unity import UnityEnv
