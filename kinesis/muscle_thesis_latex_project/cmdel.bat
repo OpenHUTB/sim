8685b55d4d6d72bebd7205516473047b28004011
mhc-2026 <2064969199@qq.com>
2026-06-15 13:45:25 +0800

    Delete kinesis/muscle_thesis_latex_project/cmdel.bat

--- kinesis/muscle_thesis_latex_project/cmdel.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-REM 清理 LaTeX 编译产生的临时文件，不删除 tex、cls、bib、图片和最终 PDF。
-del /a /f *.aux *.bbl *.bcf *.blg *.fdb_latexmk *.fls *.log *.out *.run.xml *.toc *.xdv *.synctex.gz 2>nul
-del /a /f content\*.aux 2>nul
